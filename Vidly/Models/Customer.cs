aa48ef4f1cffb6d63a15dfe148223df9e9b5e293
luka <rawwer@gmail.com>
2016-11-26 19:22:48 +0100

    Section 2 Exercise 2

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -12,6 +12,7 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        public DateTime? Birthdate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType  MemeberShipType { get; set; }
         public byte MemberShipTypeId { get; set; }
